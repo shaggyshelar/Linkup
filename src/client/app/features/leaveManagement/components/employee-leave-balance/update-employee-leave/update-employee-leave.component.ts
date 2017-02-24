34c7604083e3bca89f00fd05103dac070682dc6d
salauddinshaikh <sagar.shelar@eternussolutions.com>
2017-02-24 12:56:01 +0530

    form validation

--- src/client/app/features/leaveManagement/components/employee-leave-balance/update-employee-leave/update-employee-leave.component.ts
+++ src/client/app/features/leaveManagement/components/employee-leave-balance/update-employee-leave/update-employee-leave.component.ts
@@ -85,4 +85,12 @@ export class UpdateEmployeeLeaveComponent implements OnInit {
        this.cancel();
     });
   }
+  isNumberKey(event:any) {
+      var charCode = (event.which) ? event.which : event.keyCode;
+          if (charCode !== 46 && charCode > 31
+            && (charCode < 48 || charCode > 57))
+             return false;
+
+          return true;
+  }
 }
--- src/client/app/features/leaveManagement/components/resigned-employee-leave/update-resigned-employee-leave/update-resigned-employee-leave.component.ts
+++ src/client/app/features/leaveManagement/components/resigned-employee-leave/update-resigned-employee-leave/update-resigned-employee-leave.component.ts
@@ -65,4 +65,11 @@ export class UpdateResignedEmployeeComponent implements OnInit {
        this.cancel();
     });
   }
+  isNumberKey(event:any) {
+      var charCode = (event.which) ? event.which : event.keyCode;
+      if (charCode !== 46 && charCode > 31
+          && (charCode < 48 || charCode > 57))
+            return false;
+      return true;
+  }
 }

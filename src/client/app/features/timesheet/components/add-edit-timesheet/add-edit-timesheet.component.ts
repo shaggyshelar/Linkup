25a9ff338bae2436ecfc14e85379212e0b0961e1
salauddinshaikh <sagar.shelar@eternussolutions.com>
2017-03-14 19:31:59 +0530

    edit time sheet ui

--- src/client/app/features/timesheet/components/add-edit-timesheet/add-edit-timesheet.component.ts
+++ src/client/app/features/timesheet/components/add-edit-timesheet/add-edit-timesheet.component.ts
@@ -32,6 +32,7 @@ export class AddEditTimesheetComponent implements OnInit {
   totalhours: any;
   notes: string = '';
   routeParam: string;
+  timesheetStatus: string = 'New';
   constructor(
     private projectService: ProjectService,
     private phasesService: PhasesService,
@@ -71,6 +72,10 @@ export class AddEditTimesheetComponent implements OnInit {
   getTimesheetForEdit() {
     this.timesheetService.getTimesheetByID(this.routeParam).subscribe((res: any) => {
       this.timesheetList = res.Timesheets;
+      this.weekStartDate = res.StartDate;
+      this.weekEndDate = res.EndDate;
+      this.timesheetStatus = res.SubmittedStatus;
+      this.setTotal(res);
       for (let i = 0; i < this.timesheetList.length; i++) {
         let project = _.find(this.projectList, function (item) {
           return item.value !== null && item.value.ID === res.Timesheets[i].Project.ID;
@@ -79,19 +84,30 @@ export class AddEditTimesheetComponent implements OnInit {
         this.onProjectChange(project.value, i);
       }
       console.log(res);
-      this.weekStartDate=res.StartDate;
-      this.weekEndDate=res.EndDate;
     });
+  }
+  setTotal(total: any) {
+    this.totalhours = {
+      TotalhrsFriday: total.TotalhrsFriday,
+      TotalhrsMonday: total.TotalhrsMonday,
+      TotalhrsSaturday: total.TotalhrsSaturday,
+      TotalhrsSunday: total.TotalhrsSunday,
+      TotalhrsThursday: total.TotalhrsThursday,
+      TotalhrsTuesday: total.TotalhrsTuesday,
+      TotalhrsWednesday: total.TotalhrsWednesday,
+      TotalhrsTimesheet: total.TotalhrsTimesheet,
+    };
   }
   initTotalHour() {
     this.totalhours = {
-      MonTotal: 0,
-      TueTotal: 0,
-      WedTotal: 0,
-      ThurTotal: 0,
-      FriTotal: 0,
-      SatTotal: 0,
-      SunTotal: 0,
+      TotalhrsFriday: 0,
+      TotalhrsMonday: 0,
+      TotalhrsSaturday: 0,
+      TotalhrsSunday: 0,
+      TotalhrsThursday: 0,
+      TotalhrsTuesday: 0,
+      TotalhrsWednesday: 0,
+      TotalhrsTimesheet: 0,
       Total: 0
     };
   }
@@ -187,7 +203,9 @@ export class AddEditTimesheetComponent implements OnInit {
     }
     return false;
   }
-
+  onCancel() {
+    this.router.navigate(['/timesheet/my']);
+  }
   checkDescription() {
     let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     for (let i = 0; i < this.timesheetList.length; i++) {
@@ -211,19 +229,19 @@ export class AddEditTimesheetComponent implements OnInit {
     return true;
   }
   checkTotalHours() {
-    if (moment(this.totalhours.MonTotal, 'HH:mm').diff(moment('8:00', 'HH:mm')) < 0) {
+    if (moment(this.totalhours.TotalhrsMonday, 'HH:mm').diff(moment('8:00', 'HH:mm')) < 0) {
       return false;
     }
-    if (moment(this.totalhours.TueTotal, 'HH:mm').diff(moment('8:00', 'HH:mm')) < 0) {
+    if (moment(this.totalhours.TotalhrsTuesday, 'HH:mm').diff(moment('8:00', 'HH:mm')) < 0) {
       return false;
     }
-    if (moment(this.totalhours.WedTotal, 'HH:mm').diff(moment('8:00', 'HH:mm')) < 0) {
+    if (moment(this.totalhours.TotalhrsWednesday, 'HH:mm').diff(moment('8:00', 'HH:mm')) < 0) {
       return false;
     }
-    if (moment(this.totalhours.ThurTotal, 'HH:mm').diff(moment('8:00', 'HH:mm')) < 0) {
+    if (moment(this.totalhours.TotalhrsThursday, 'HH:mm').diff(moment('8:00', 'HH:mm')) < 0) {
       return false;
     }
-    if (moment(this.totalhours.FriTotal, 'HH:mm').diff(moment('8:00', 'HH:mm')) < 0) {
+    if (moment(this.totalhours.TotalhrsFriday, 'HH:mm').diff(moment('8:00', 'HH:mm')) < 0) {
       return false;
     }
     return true;
@@ -247,47 +265,55 @@ export class AddEditTimesheetComponent implements OnInit {
     this.initTotalHour();
     for (let i = 0; i < this.timesheetList.length; i++) {
       if (this.timesheetList[i].Mondaynbhrs && this.timesheetList[i].Mondaynbhrs !== null && this.timesheetList[i].Mondaynbhrs.length > 0) {
-        this.totalhours.MonTotal = moment(moment(this.totalhours.MonTotal, 'HH:mm').add(moment(this.timesheetList[i].Mondaynbhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Mondaynbhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
+        this.totalhours.TotalhrsMonday = moment(moment(this.totalhours.TotalhrsMonday, 'HH:mm').add(moment(this.timesheetList[i].Mondaynbhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Mondaynbhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
       }
       if (this.timesheetList[i].Mondayhrs && this.timesheetList[i].Mondayhrs !== null && this.timesheetList[i].Mondayhrs.length > 0) {
-        this.totalhours.MonTotal = moment(moment(this.totalhours.MonTotal, 'HH:mm').add(moment(this.timesheetList[i].Mondayhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Mondayhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
+        this.totalhours.TotalhrsMonday = moment(moment(this.totalhours.TotalhrsMonday, 'HH:mm').add(moment(this.timesheetList[i].Mondayhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Mondayhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
       }
       if (this.timesheetList[i].Tuesdayhrs && this.timesheetList[i].Tuesdayhrs !== null && this.timesheetList[i].Tuesdayhrs.length > 0) {
-        this.totalhours.TueTotal = moment(moment(this.totalhours.TueTotal, 'HH:mm').add(moment(this.timesheetList[i].Tuesdayhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Tuesdayhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
+        this.totalhours.TotalhrsTuesday = moment(moment(this.totalhours.TotalhrsTuesday, 'HH:mm').add(moment(this.timesheetList[i].Tuesdayhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Tuesdayhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
       }
       if (this.timesheetList[i].Tuesdaynbhrs && this.timesheetList[i].Tuesdaynbhrs !== null && this.timesheetList[i].Tuesdaynbhrs.length > 0) {
-        this.totalhours.TueTotal = moment(moment(this.totalhours.TueTotal, 'HH:mm').add(moment(this.timesheetList[i].Tuesdaynbhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Tuesdaynbhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
+        this.totalhours.TotalhrsTuesday = moment(moment(this.totalhours.TotalhrsTuesday, 'HH:mm').add(moment(this.timesheetList[i].Tuesdaynbhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Tuesdaynbhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
       }
       if (this.timesheetList[i].Wednesdaynbhrs && this.timesheetList[i].Wednesdaynbhrs !== null && this.timesheetList[i].Wednesdaynbhrs.length > 0) {
-        this.totalhours.WedTotal = moment(moment(this.totalhours.WedTotal, 'HH:mm').add(moment(this.timesheetList[i].Wednesdaynbhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Wednesdaynbhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
+        this.totalhours.TotalhrsWednesday = moment(moment(this.totalhours.TotalhrsWednesday, 'HH:mm').add(moment(this.timesheetList[i].Wednesdaynbhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Wednesdaynbhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
       }
       if (this.timesheetList[i].Wednesdayhrs && this.timesheetList[i].Wednesdayhrs !== null && this.timesheetList[i].Wednesdayhrs.length > 0) {
-        this.totalhours.WedTotal = moment(moment(this.totalhours.WedTotal, 'HH:mm').add(moment(this.timesheetList[i].Wednesdayhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Wednesdayhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
+        this.totalhours.TotalhrsWednesday = moment(moment(this.totalhours.TotalhrsWednesday, 'HH:mm').add(moment(this.timesheetList[i].Wednesdayhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Wednesdayhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
       }
       if (this.timesheetList[i].Thursdaynbhrs && this.timesheetList[i].Thursdaynbhrs !== null && this.timesheetList[i].Thursdaynbhrs.length > 0) {
-        this.totalhours.ThurTotal = moment(moment(this.totalhours.ThurTotal, 'HH:mm').add(moment(this.timesheetList[i].Thursdaynbhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Thursdaynbhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
+        this.totalhours.TotalhrsThursday = moment(moment(this.totalhours.TotalhrsThursday, 'HH:mm').add(moment(this.timesheetList[i].Thursdaynbhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Thursdaynbhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
       }
       if (this.timesheetList[i].Thursdayhrs && this.timesheetList[i].Thursdayhrs !== null && this.timesheetList[i].Thursdayhrs.length > 0) {
-        this.totalhours.ThurTotal = moment(moment(this.totalhours.ThurTotal, 'HH:mm').add(moment(this.timesheetList[i].Thursdayhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Thursdayhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
+        this.totalhours.TotalhrsThursday = moment(moment(this.totalhours.TotalhrsThursday, 'HH:mm').add(moment(this.timesheetList[i].Thursdayhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Thursdayhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
       }
       if (this.timesheetList[i].Fridaynbhrs && this.timesheetList[i].Fridaynbhrs !== null && this.timesheetList[i].Fridaynbhrs.length > 0) {
-        this.totalhours.FriTotal = moment(moment(this.totalhours.FriTotal, 'HH:mm').add(moment(this.timesheetList[i].Fridaynbhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Fridaynbhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
+        this.totalhours.TotalhrsFriday = moment(moment(this.totalhours.TotalhrsFriday, 'HH:mm').add(moment(this.timesheetList[i].Fridaynbhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Fridaynbhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
       }
       if (this.timesheetList[i].Fridayhrs && this.timesheetList[i].Fridayhrs !== null && this.timesheetList[i].Fridayhrs.length > 0) {
-        this.totalhours.FriTotal = moment(moment(this.totalhours.FriTotal, 'HH:mm').add(moment(this.timesheetList[i].Fridayhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Fridayhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
+        this.totalhours.TotalhrsFriday = moment(moment(this.totalhours.TotalhrsFriday, 'HH:mm').add(moment(this.timesheetList[i].Fridayhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Fridayhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
       }
       if (this.timesheetList[i].Saturdaynbhrs && this.timesheetList[i].Saturdaynbhrs !== null && this.timesheetList[i].Saturdaynbhrs.length > 0) {
-        this.totalhours.SatTotal = moment(moment(this.totalhours.SatTotal, 'HH:mm').add(moment(this.timesheetList[i].Saturdaynbhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Saturdaynbhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
+        this.totalhours.TotalhrsSaturday = moment(moment(this.totalhours.TotalhrsSaturday, 'HH:mm').add(moment(this.timesheetList[i].Saturdaynbhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Saturdaynbhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
       }
       if (this.timesheetList[i].Saturdayhrs && this.timesheetList[i].Saturdayhrs !== null && this.timesheetList[i].Saturdayhrs.length > 0) {
-        this.totalhours.SatTotal = moment(moment(this.totalhours.SatTotal, 'HH:mm').add(moment(this.timesheetList[i].Saturdayhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Saturdayhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
+        this.totalhours.TotalhrsSaturday = moment(moment(this.totalhours.TotalhrsSaturday, 'HH:mm').add(moment(this.timesheetList[i].Saturdayhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Saturdayhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
       }
       if (this.timesheetList[i].Sundaynbhrs && this.timesheetList[i].Sundaynbhrs !== null && this.timesheetList[i].Sundaynbhrs.length > 0) {
-        this.totalhours.SunTotal = moment(moment(this.totalhours.SunTotal, 'HH:mm').add(moment(this.timesheetList[i].Sundaynbhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Sundaynbhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
+        this.totalhours.TotalhrsSunday = moment(moment(this.totalhours.TotalhrsSunday, 'HH:mm').add(moment(this.timesheetList[i].Sundaynbhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Sundaynbhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
       }
       if (this.timesheetList[i].Sundayhrs && this.timesheetList[i].Sundayhrs !== null && this.timesheetList[i].Sundayhrs.length > 0) {
-        this.totalhours.SunTotal = moment(moment(this.totalhours.SunTotal, 'HH:mm').add(moment(this.timesheetList[i].Sundayhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Sundayhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
+        this.totalhours.TotalhrsSunday = moment(moment(this.totalhours.TotalhrsSunday, 'HH:mm').add(moment(this.timesheetList[i].Sundayhrs, 'HH:mm').hours() * 60 + moment(this.timesheetList[i].Sundayhrs, 'HH:mm').minutes(), 'minutes')).format('HH:mm');
       }
     }
+    this.totalhours.TotalhrsTimesheet = this.totalhours.TotalhrsFriday +
+      this.totalhours.TotalhrsMonday +
+      this.totalhours.TotalhrsSaturday +
+      this.totalhours.TotalhrsSunday +
+      this.totalhours.TotalhrsThursday +
+      this.totalhours.TotalhrsTuesday +
+      this.totalhours.TotalhrsWednesday +
+      this.totalhours.TotalhrsTimesheet;
   }
 }

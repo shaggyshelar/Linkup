94b796ad73842387b8799894e34692fe2fe971f9
salauddinshaikh <sagar.shelar@eternussolutions.com>
2017-03-15 15:09:26 +0530

    added hashstratergy for routing

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { APP_BASE_HREF } from '@angular/common';
+import { APP_BASE_HREF, HashLocationStrategy, Location, LocationStrategy } from '@angular/common';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -14,9 +14,11 @@ import { FeaturesModule } from './features/index';
   //imports: [BrowserModule, HttpModule, AppRoutingModule, AboutModule, HomeModule, SharedModule.forRoot()],
   imports: [BrowserModule, HttpModule, FeaturesModule, AppRoutingModule, SharedModule.forRoot()],
   declarations: [AppComponent],
-  providers: [{
+  providers: [
+   Location, {provide: LocationStrategy, useClass: HashLocationStrategy},
+    {
     provide: APP_BASE_HREF,
-    useValue: '<%= APP_BASE %>'
+    useValue: '<%= APP_BASE %>',
   }],
   bootstrap: [AppComponent]
 
